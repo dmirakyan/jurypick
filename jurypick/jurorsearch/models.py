f03e99221f207cdca81a4f171c8eb2ded2aae6cb
dmirakyan <damirakyan@gmail.com>
2020-10-29 22:31:04 -0600

    added logout functionality

--- jurypick/jurorsearch/models.py
+++ jurypick/jurorsearch/models.py
@@ -3,18 +3,18 @@ from django.db import models
 # Create your models here.
 class Query(models.Model):
     search_id = models.AutoField(primary_key=True)
-    first_name = models.CharField(max_length=128)
-    middle_name = models.CharField(max_length=128)
-    last_name = models.CharField(max_length=128)
-    address = models.CharField(max_length=128)
-    city = models.CharField(max_length=128)
-    state = models.CharField(max_length=128)
-    zip_code = models.CharField(max_length=128)
-    email = models.CharField(max_length=128)
-    phone = models.CharField(max_length=128)
-    birth_date = models.CharField(max_length=128)
+    first_name = models.CharField(max_length=128, null=True)
+    middle_name = models.CharField(max_length=128, null=True)
+    last_name = models.CharField(max_length=128, null=True)
+    address = models.CharField(max_length=128, null=True)
+    city = models.CharField(max_length=128, null=True)
+    state = models.CharField(max_length=128, null=True)
+    zip_code = models.CharField(max_length=128, null=True)
+    email = models.CharField(max_length=128, null=True)
+    phone = models.CharField(max_length=128, null=True)
+    birth_date = models.CharField(max_length=128, null=True)
     def __str__(self):  
-        return self.first_name
+        return self.first_name + self.last_name
 
 class Human(models.Model):
     search_id = models.OneToOneField(Query, on_delete=models.CASCADE)
